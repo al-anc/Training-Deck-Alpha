aa83e9935f66a2dce15b589e6a4d3a397893e548
Alan C <77475733+al-anc@users.noreply.github.com>
2022-04-17 17:49:48 -0400

    Health and Timer UI on all Levels

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    public GameObject healthDisplay;
     public int currentHealth = 5;
     public int maxHealth = 5;
     public bool isDead = false;
@@ -11,13 +14,21 @@ public class Health : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        healthDisplay.GetComponent<Text>().text = "Health = " + currentHealth;
     }
+
+    void Update()
+    {
+        healthDisplay.GetComponent<Text>().text = "Health = " + currentHealth;
+    }
+
     public void Damage(int d) 
     {
         currentHealth -= d;
         if(currentHealth <= 0)
         {
             isDead = true;
+            SceneManager.LoadScene(2);
         }
     }
     // Update is called once per frame
