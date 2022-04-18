815b3b2ccaa9a0dd6f20551aa9d5bdcde0de707a
crunchb1244 <crunchb1244@gmail.com>
2022-04-17 21:07:35 -0400

    shit's fucked

---
+++ Assets/scripts/PlayerShoot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject player; 
+    public GameObject projectilePrefab;
+    public bool canShoot = true;
+    public float shootTime = 1;
+    // Start is called before the first frame update
+
+    void CanShootAgain()
+    {
+        canShoot = true;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.F))
+       {
+          Instantiate(projectilePrefab, transform.position + transform.forward, transform.rotation);
+        //instantiate bullet transform.lookatplayer transform.look at first 
+        Debug.Log("pew");
+        canShoot = false;
+        Invoke("CanShootAgain",shootTime);
+       }
+        if(canShoot == false)
+        {
+            return;
+        }
+        
+    }
+    
+}
