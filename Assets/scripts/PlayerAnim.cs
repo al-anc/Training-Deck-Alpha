d6453dfe16867b2234241e4d42eb0180a2a2b831
murmur-myst <jon3murray@gmail.com>
2022-04-28 17:18:36 -0400

    animations and controller done

--- Assets/scripts/PlayerAnim.cs
+++ Assets/scripts/PlayerAnim.cs
@@ -8,6 +8,8 @@ public class PlayerAnim : MonoBehaviour
     private bool isJumping;
     private bool IsCrouching;
     private bool crouched = false;
+    public int isWalking = 0;
+    public int isStrafing = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,38 +20,64 @@ public class PlayerAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //
+        float horizontal = Input.GetAxisRaw("MoveHorizontal");
+        float vertical = Input.GetAxisRaw("MoveVertical");
+        //Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (horizontal > 0.15f){
+            isStrafing = 1;
+        }
+        else if (horizontal < -0.15f){
+            isStrafing = -1;
+        }
+        else{
+            isStrafing = 0;
+        }
 
-        if(Input.GetKeyDown(KeyCode.W)){
+        if(vertical > 0.15f){
+            isWalking = 1;
+        }
+        else if (vertical < -0.15f){
+            isWalking = -1;
+        }
+        else
+        {
+            isWalking = 0;
+        }
+        //
+
+        if(Input.GetKeyDown(KeyCode.W) || isWalking == 1){
             anim.SetInteger("ZSpeed", 1);
         }
-        else if (Input.GetKeyDown(KeyCode.S)){
+        else if (Input.GetKeyDown(KeyCode.S) || isWalking == -1){
             anim.SetInteger("ZSpeed", -1);
         }
-        else if (Input.GetKeyUp(KeyCode.W)){
+        else if (Input.GetKeyUp(KeyCode.W) || isWalking == 0){
             anim.SetInteger("ZSpeed", 0);
         }
-        else if (Input.GetKeyUp(KeyCode.S)){
+        else if (Input.GetKeyUp(KeyCode.S) || isWalking == 0 ){
             anim.SetInteger("ZSpeed", 0);
         }
-        else if (Input.GetKeyDown(KeyCode.A)){
+
+        if (Input.GetKeyDown(KeyCode.A) || isStrafing == -1){
             anim.SetInteger("XSpeed", -1);
         }
-        else if(Input.GetKeyUp(KeyCode.A)){
+        else if(Input.GetKeyUp(KeyCode.A) || isStrafing == 0){
             anim.SetInteger("XSpeed", 0);
         }
-        else if (Input.GetKeyDown(KeyCode.D)){
+        else if (Input.GetKeyDown(KeyCode.D) || isStrafing == 1){
             anim.SetInteger("XSpeed", 1);
         }
-        else if(Input.GetKeyUp(KeyCode.D)){
+        else if(Input.GetKeyUp(KeyCode.D) || isStrafing == 0){
             anim.SetInteger("XSpeed", 0);
         }
         
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.JoystickButton0)){
             anim.SetBool("IsJumping", true);
         }
 
-        if(Input.GetKeyUp(KeyCode.Space)){
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.JoystickButton0)){
             anim.SetBool("IsJumping", false);
         }
 
@@ -61,7 +89,7 @@ public class PlayerAnim : MonoBehaviour
                 anim.SetBool("IsCrouching", false);
         }*/
         //adapted from crouch script
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.JoystickButton8))
         {
             crouched = !crouched;
        
